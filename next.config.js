a06d7286be1f97392819263ddfe5b919a8c55b38
Kavi <60553508+K4v1@users.noreply.github.com>
2025-05-25 10:44:35 +0200

    chore: change build output dir to docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'docs',
   basePath: '/kp-personal-website',
   assetPrefix: '/kp-personal-website',
 };
