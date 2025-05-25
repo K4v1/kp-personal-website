e91756c81bfac2fa175a98a40ef3b332646dea4f
Kavi <60553508+K4v1@users.noreply.github.com>
2025-05-25 22:19:06 +0200

    Improve layout and styles

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   distDir: 'docs',
   basePath: '/kp-personal-website',
   assetPrefix: '/kp-personal-website',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
